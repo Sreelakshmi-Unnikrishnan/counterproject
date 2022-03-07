15b887b2102bc0ccac28b9caecbf7b126ae9855b
sreelakshmi <sreelakshmialeesha@gmail.com>
2022-03-07 10:34:45 +0530

    counter project implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import randomColor from "randomcolor";
+import "./style.css";
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor()
+  {
+    super()
+    this.state = {
+      count : 0,
+      color:""
+    }
+    this.incrementHandler = this.incrementHandler.bind(this)
+    // this.decrementHandler = this.decrementHandler.bind(this)
+  }
+  componentDidMount(){
+    this.setState({color:"red"})
+    console.log("mounted and color changed")
+  }
+
+  componentDidUpdate(prevProps,prevState){
+    console.log("1-current count state",this.state.count);
+    console.log("2-previous count state",prevState.count);
+    if(prevState.count !== this.state.count)
+    {
+      this.setState({color:randomColor()})
+    }
+    
+  }
+
+  incrementHandler(){
+    this.setState((prevState)=>{
+      return {
+        count : prevState.count + 1
+      }
+    })
+  }
+  decrementHandler =() => {
+    this.setState((prevState)=>{
+      return {
+        count : prevState.count - 1
+      }
+    })
+  }
+
+  render(){
+      return(
+        <div className="counter">
+          {console.log("rendering completed")}
+          <h1 style={{color:this.state.color}}>{this.state.count}</h1>
+          <div className="btn-actions">
+            <button onClick={this.incrementHandler}>Increment</button>
+            <button onClick={this.decrementHandler}>Decrement</button>
+          </div>
+        </div>
+      )
+  }
 }
 
 export default App;
